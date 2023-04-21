6466ef374cabd27d7c40ecbacb602227a888d508
Paul Lipp <paullipp@Pauls-MacBook-Pro-2.local>
2023-04-21 01:19:26 -0700

    removing secrets

--- songs.js
+++ songs.js
@@ -1,3 +1,5 @@
 const creds = require('./secrets.json')
 const axios = require('axios')
 const QueryString = require('qs')
+
+posturl = 'https://api.spotify.com/v1/users/' + creds['user_id'] + '/tracks'
